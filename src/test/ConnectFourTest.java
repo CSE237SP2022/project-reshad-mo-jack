27b43aba0e501e16730ea1309a3d67c6b2316a41
reshadadada <a.hamauon@wustl.edu>
2022-03-28 14:22:08 -0500

    added 2/3 tests

--- src/test/ConnectFourTest.java
+++ src/test/ConnectFourTest.java
@@ -2,4 +2,23 @@ package test;
 
 public class ConnectFourTest {
 
+    /*@Test
+	void addPieceToFullColumn() {
+        
+        
+    }*/
+
+    @Test
+    void addPieceBelowBoard() {
+        ConnectFour board = new Board();
+        boolean wasValid = Board.validMove(0);
+        assertEquals(wasValid,False);
+    }
+
+    @Test
+    void addPiecePastBoard() {
+        ConnectFour board = new Board();
+        boolean wasValid = Board.validMove(9);
+        assertEquals(wasValid,False);
+    }
 }
